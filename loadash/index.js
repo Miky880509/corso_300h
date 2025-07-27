c46ff260fe46eed075c09af5b4cfd71afac13864
Miky880509 <micaelatopazi@gmail.com>
2025-07-27 20:09:42 +0200

    aggiunta file

---
+++ loadash/index.js
@@ -0,0 +1,15 @@
+
+import { chunk, difference, xorArrays } from "./my-loadash.js";
+import _ from "lodash";
+// import {strict as assert} from "node:assert"
+
+const my_array= [50, 60, 10, 25, 30, 45, 5];
+const arrayword=["ciao","come", "stai"]
+const arraydobble=["ciao","ciao", "viola","viola", 10,10]
+const arraycolor=["rosso","giallo","blu","viola","bianco","nero"];
+
+
+const [expected, actual] = [_.chunk(my_array, 3), chunk(my_array, 3)]
+console.log(expected, '\n', actual)
+
+
